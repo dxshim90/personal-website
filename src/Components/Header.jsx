79606ee701c79219cbe524e31030521d47425a4a
dxshim90 <dxshim90@gmail.com>
2019-11-01 19:55:17 +0000

    navbar cteated

---
+++ src/Components/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+const Header = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand style={{ color: "white" }} href="/">
+          Daniel Shimield
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink style={{ color: "white" }} href="/components/">
+                Projects
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                style={{ color: "white" }}
+                href="https://github.com/reactstrap/reactstrap"
+              >
+                About Me
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink style={{ color: "white" }} href="/components/">
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
